a4863666396025ff2bc5c72e06fc3f6973b980d3
tshemsedinov <timur.shemsedinov@gmail.com>
2018-12-19 14:52:28 +0200

    Improve example

--- JavaScript/1-global.js
+++ JavaScript/1-global.js
@@ -5,25 +5,30 @@ let k = 0;
 
 const bytesToMb = bytes => Math.round(bytes / 1000, 2) / 1000;
 
-setInterval(() => {
+const timer = setInterval(() => {
   k++;
   const key = 'globalVariable' + k;
   global[key] = new Array(1000).fill(key);
-}, 10);
+}, 5);
 
 setInterval(() => {
   console.clear();
   const usage = process.memoryUsage();
   const row = {
-    rss: bytesToMb(usage.rss),
-    heapTotal: bytesToMb(usage.heapTotal),
-    heapUsed: bytesToMb(usage.heapUsed),
-    external: usage.external
+    rss: bytesToMb(usage.rss), // process resident set size
+    heapTotal: bytesToMb(usage.heapTotal), // v8 heap allocated
+    heapUsed: bytesToMb(usage.heapUsed), // v8 heap used
+    external: bytesToMb(usage.external), // c++ allocated
+    stack: bytesToMb(usage.rss - usage.heapTotal), // stack
   };
   memory.push(row);
   console.table(memory);
 }, 1000);
 
 setTimeout(() => {
-  process.exit(0);
+  clearInterval(timer);
 }, 10000);
+
+setTimeout(() => {
+  process.exit(0);
+}, 15000);
